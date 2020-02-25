557b5adde1bc7baeb5e723358e348f01e175e8bb
ivanfermena <2929269Mayo!>
2020-02-25 17:41:44 +0100

    Refactorizacion de carpetas

---
+++ setup.py
@@ -0,0 +1,7 @@
+# La idea es que esta clase este configurada para lanzar todos los informes o generar un menu que puedas escoger el informe determinado
+
+
+if __name__== "__main__":
+    
+    print("Start analysis...\n\r")
+
